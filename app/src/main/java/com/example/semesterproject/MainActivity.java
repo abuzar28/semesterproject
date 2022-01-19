59061ce9a412bfaae308200435c5312ba57b1878
abuzar.bit <abuzarrehan95@gmail.com>
2022-01-20 01:26:28 +0500

    fire base connection esteblishment

--- app/src/main/java/com/example/semesterproject/MainActivity.java
+++ app/src/main/java/com/example/semesterproject/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.semesterproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        switch (item.getItemId()){
+            case R.id.account:
+                Toast.makeText(this,"account",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.notification:
+                Toast.makeText(this,"notification",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
